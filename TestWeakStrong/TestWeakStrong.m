d70bde7213027765b56b649cccb0266bbf2fa722
Bill liu <liumin2011@gmail.com>
2016-05-05 10:56:05 +0800

    加入属性测试

--- TestWeakStrong/TestWeakStrong.m
+++ TestWeakStrong/TestWeakStrong.m
@@ -8,19 +8,31 @@
 
 #import "TestWeakStrong.h"
 
+@interface TestWeakStrong()
+@property (nonatomic,strong) NSString * orginal3;
+@property (nonatomic,weak) NSString * weakOfOrginal3;
+
+@end
+
+
 @implementation TestWeakStrong
 
 -(instancetype)init{
     self = [super init];
     
-    if (self) {
+    if (self) {        
+        self.orginal3 = @"orginal3";
+        self.weakOfOrginal3 = _orginal3;
+        self.orginal3 = nil;
+        NSLog(@"原数据3:%@\n\n",self.weakOfOrginal3);
+        
         NSString * orginal = @"orginal";
-        NSString * pointO = @"orginal2";
+        NSString * orginal2 = @"orginal2";
         __weak NSString * weakPointO = orginal;
-        __weak NSString * weakPointPointO = pointO;
+        __weak NSString * weakPointPointO = weakPointO;
         
         NSLog(@"原数据:%@ %p",orginal,&orginal);
-        NSLog(@"指向原数据:%@ %p",pointO,&pointO);
+        NSLog(@"原数据2:%@ %p",orginal2,&orginal2);
         NSLog(@"弱引用指向原数据:%@ %p",weakPointO,&weakPointO);
         NSLog(@"弱引用指向指向原数据:%@ %p\n\n",weakPointPointO,&weakPointPointO);
         
@@ -28,7 +40,7 @@
         NSLog(@"更新值后\n\n");
         orginal = nil;
         NSLog(@"原数据:%@ %p",orginal,&orginal);
-        NSLog(@"指向原数据:%@ %p",pointO,&pointO);
+        NSLog(@"原数据2:%@ %p",orginal2,&orginal2);
         NSLog(@"弱引用指向原数据:%@ %p",weakPointO,&weakPointO);
         NSLog(@"弱引用指向指向原数据:%@ %p\n\n",weakPointPointO,&weakPointPointO);
         
