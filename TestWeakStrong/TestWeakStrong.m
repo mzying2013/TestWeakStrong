46a08df7c5cdd173c7b2e749bcda9a1068cdd8c4
liumin_mbp <317590898@qq.com>
2016-08-22 23:27:01 +0800

    重新调整代码，重现weak, strong的关系

--- TestWeakStrong/TestWeakStrong.m
+++ TestWeakStrong/TestWeakStrong.m
@@ -8,6 +8,8 @@
 
 #import "TestWeakStrong.h"
 
+#define TLog(name,Obj) {NSLog(@"变量内存地址：%p, 变量值：%p, 指向对象值：%@, --> %@",&Obj,Obj,Obj,name);}
+
 @interface TestWeakStrong()
 @property (nonatomic,strong) NSString * orginal3;
 @property (nonatomic,weak) NSString * weakOfOrginal3;
@@ -20,7 +22,32 @@
 -(instancetype)init{
     self = [super init];
     
-    if (self) {        
+    if (self) {
+        NSLog(@"========start==================");
+        
+        //不管weak还是strong，变量内存地址都不一样。变量值才一样
+        NSObject *oobj = [NSObject new];
+        __weak NSObject * weakObj = oobj;
+        NSObject * strongObj = oobj;
+        TLog(@"oobj", oobj);
+        TLog(@"weakobj", weakObj);
+        TLog(@"strongObj", strongObj);
+        //即使oobj置为空，但是变量值任然后strongObj指向，所以weakObj不会自动释放
+        oobj = nil;
+        NSLog(@"oobj set nil");
+        TLog(@"oobj", oobj);
+        TLog(@"weakobj", weakObj);
+        TLog(@"strongObj", strongObj);
+        strongObj = nil;
+        NSLog(@"strongObj set nil");
+        TLog(@"oobj", oobj);
+        TLog(@"weakobj", weakObj);
+        TLog(@"strongObj", strongObj);
+        
+        NSLog(@"========end====================");
+        
+        
+        /*
         self.orginal3 = @"orginal3";
         self.weakOfOrginal3 = _orginal3;
         self.orginal3 = nil;
@@ -43,7 +70,7 @@
         NSLog(@"原数据2:%@ %p",orginal2,&orginal2);
         NSLog(@"弱引用指向原数据:%@ %p",weakPointO,&weakPointO);
         NSLog(@"弱引用指向指向原数据:%@ %p\n\n",weakPointPointO,&weakPointPointO);
-        
+       */
     }
     
     return self;
